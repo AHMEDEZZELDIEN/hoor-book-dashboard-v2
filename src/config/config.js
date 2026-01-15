893b0283fbf9132bc75001e253389ef9b65e29c0
Ahmed Ezz <ahmed.ezz3389@gmail.com>
2026-01-15 20:12:53 +0200

    Feat: change ip of domain

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,10 @@
-//export const BASE_API = 'http://143.110.170.3';
-export const BASE_API = 'https://hoorbookapp.com';
+export const BASE_API = 'http://185.175.208.37';
+// export const BASE_API = 'https://hoorbookapp.com';
+// export const BASE_URL = 'https://hoorbookapp.com/';
 // export const BASE_API = 'https://ezz.hoorbookapp.com';
+// export const BASE_URL = 'https://ezz.hoorbookapp.com/';
 // export const BASE_API = 'http://hoorbook.loc';
+// export const BASE_URL = 'http://hoorbook.loc/';
 
 export const lookups = [
     'body-specifications',
